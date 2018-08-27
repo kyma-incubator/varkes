e43ab4e4dd4783b21114af96c5f1ab51c53982c3
Atakan Yenel <atakan.yenel@sap.com>
2018-08-27 14:00:29 +0200

    add docker

---
+++ app-connector/Readme.md
@@ -0,0 +1,8 @@
+# App Connector
+
+For running with docker,
+
+- `docker build -t harmonia/server .`
+- `docker run -p 3000:3000 harmonia/server`
+
+Go to localhost:3000.
--- app-connector/server/server.js
+++ app-connector/server/server.js
@@ -1,11 +1,21 @@
-var app = require("express")
+var express = require("express")
 var connector = require("./connector")
-var url
-app.listen(3000,function(){
-app.emit('server started')
-app.post('/startConnection/',function(req,res){
 
-console.log(res.body);
+
+var app = express()
+var url
+app.post('/startConnection/', function (req, res) {
+
+    console.log(res.body);
 });
+
+app.get("/", function (req, res) {
+    res.end("testing")
+})
+var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
 
 });
