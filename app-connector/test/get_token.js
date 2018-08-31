f967e8d3737452eaa5110fb4c55afb7509541b7d
Atakan Yenel <atakan.yenel@sap.com>
2018-08-31 14:53:10 +0200

    add basic ui

--- app-connector/test/get_token.js
+++ app-connector/test/get_token.js
@@ -5,12 +5,6 @@ request.post(
     "http://127.0.0.1:8080/v1/remoteenvironments/hmc-default/tokens",
     (error, response, body) => {
         resp = JSON.parse(body)
-        console.log(body)
-        request.post(
-            "http://127.0.0.1:3000/startConn",
-            { json: resp },
-            (error, response, data) => {
-                console.log(data)
-            }
-        )
+        console.log(JSON.parse(body).url)
+
     });
