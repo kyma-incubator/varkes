84299cb6e1269ceabf68ff992e0c655f2d952c3e
Elbadrawi <omar.elbadrawi@sap.com>
2018-09-03 16:15:25 +0200

    Adding different test

--- OpenAPIMock/test/test_openapi.js
+++ OpenAPIMock/test/test_openapi.js
@@ -1,10 +1,11 @@
 var request = require('supertest');
 var server = require('../app');
+var assert = require('assert');
 describe('controllers', function () {
 
   describe('test_openapi', function () {
 
-    describe('GET cardtypes', function () {
+    describe('GET metadata', function () {
 
       it('should return response 200', function (done) {
 
@@ -13,6 +14,22 @@ describe('controllers', function () {
           .set('Accept', 'application/json')
           .expect('Content-Type', 'text/x-yaml; charset=utf-8')
           .expect(200, done)
+      });
+
+
+    });
+    describe('GET card types', function () {
+
+      it('should return an array with three items', function (done) {
+
+        request(server)
+          .get('/1/cardtypes')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', 'application/json')
+          .end(function (err, res) {
+            assert(res.body.cardTypes.length, 3);
+            done();
+          })
       });
 
 
