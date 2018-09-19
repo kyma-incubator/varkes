8e9aba9561e1483044039eea6942fa3166a923e3
Atakan Yenel <atakan.yenel@sap.com>
2018-09-19 17:12:01 +0200

    WIP: start cli task

---
+++ app-connector/cli.js
@@ -0,0 +1,101 @@
+const program = require("commander")
+const request = require("request")
+const readline = require("readline")
+const connector = require("./server/connector")
+const fs = require("fs")
+var CONFIG = require("./config")
+const path = require("path")
+console.log("CLI for Varkes App Connector")
+
+
+program
+    .version("0.0.1")
+    .option('-token, --token [tokenUrl]', "connect token for RE", '')
+    .option('--input [inputFile]', "file to register with app-connector", '')
+    .parse(process.argv)
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let keyFile, certFile
+
+getInputFile(rl, program).then(inputFile => {
+
+    const serviceMetadata = path.resolve(inputFile)
+    console.log(serviceMetadata)
+
+    if (fs.existsSync(path.resolve(CONFIG.keyDir, CONFIG.apiFile))) {
+        CONFIG.URLs = JSON.parse(fs.readFileSync(path.resolve(CONFIG.keyDir, CONFIG.apiFile)))
+        console.log("keys exist")
+        keyFile = path.resolve(CONFIG.keyDir, 'ec-default.key')
+            , certFile = path.resolve(CONFIG.keyDir, 'kyma.crt')
+
+        createServiceFromFile(JSON.parse(fs.readFileSync(serviceMetadata)))
+
+    } else {
+        getTokenUrl(rl, program).then(tokenUrl => {
+
+            createKeysFromToken(tokenUrl, urls => {
+                fs.writeFileSync(path.resolve(CONFIG.keyDir, CONFIG.apiFile), JSON.stringify(data), "utf8")
+                res.send(data)
+                CONFIG.URLs = data
+
+                createServiceFromFile(JSON.parse(fs.readFileSync(serviceMetadata)))
+
+            })
+
+        })
+    }
+})
+rl.close()
+
+function createKeysFromToken(tokenUrl, cb) {
+    connector.exportKeys(tokenUrl, (data) => cb(data))
+}
+function createServiceFromFile(serviceMetadata) {
+    console.log(serviceMetadata)
+    request.post({
+        url: CONFIG.URLs.metadataUrl,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        json: serviceMetadata,
+        agentOptions: {
+            cert: fs.readFileSync(certFile),
+            key: fs.readFileSync(keyFile)
+        }
+    }, function (error, httpResponse, body) {
+        console.log(body)
+    });
+}
+
+function getTokenUrl(rl, program) {
+
+    return new Promise((resolve, reject) => {
+        if (program.token === '') {
+
+            rl.question("Please enter your connection token: ", (answer) => {
+                resolve(answer)
+
+
+            })
+        } else { resolve(program.token) }
+    })
+}
+
+
+function getInputFile(rl, program) {
+    return new Promise((resolve, reject) => {
+        if (program.input == '') {
+            rl.question("Please enter your input file: ", (answer) => {
+
+                resolve(answer)
+
+            })
+        } else { resolve(program.input) }
+    })
+
+}
