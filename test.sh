296fbe82fdc36c875da05aa2a40d598481d186bc
Elbadrawi <omar.elbadrawi@sap.com>
2018-10-16 11:06:21 +0200

    fixing issue with getting changed directories

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 changedDir=$(git diff pr-$PULL_NUMBER master --dirstat | cut -d' ' -f3-)
-arr=$(echo $changedDir | tr " " "\n")
-
-for x in $arr
+changedDirArray=$(echo $changedDir | tr " " "\n")
+makeDirs=$(find . -type f -name '*make*' | sed -r 's|/[^/]+$||' |sort |uniq)
+makeDirsArray=$(echo $makeDir | tr " " "\n")
+echo ${myArray[@]/green//} | cut -d/ -f1 | wc -w | tr -d ' '
+for x in $changedDirArray
 do
-   if [[ "$x" == *\/* ]];
-   then
-	 echo "\"$x\""
-	 cd "$x"
-	 make ci
-   fi
+   for y in $makeDirsArray
+   do
+        if [[ "$x" == *"$y"* ]];
+        then
+            echo "\"$y\""
+            cd "$y"
+            make ci
+        fi
+    done
 done
