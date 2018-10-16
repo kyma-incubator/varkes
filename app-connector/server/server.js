904984d3d0e1c232ed4588caf75156de7cd3f72b
Atakan Yenel <atakan.yenel@sap.com>
2018-10-16 17:08:18 +0200

    connector server

--- app-connector/server/server.js
+++ app-connector/server/server.js
@@ -3,6 +3,7 @@
 var express = require("express")
 var Resource = require("express-resource")
 var connector = require("./connector")
+var request = require("request")
 var fs = require("fs")
 const path = require("path")
 
@@ -14,7 +15,7 @@ app.use(bodyParser.json());
 if (fs.existsSync(path.resolve(CONFIG.keyDir, CONFIG.apiFile))) {
     CONFIG.URLs = JSON.parse(fs.readFileSync(path.resolve(CONFIG.keyDir, CONFIG.apiFile)))
 }
-app.use(express.static('server/views'))
+app.use(express.static(path.resolve(__dirname, 'views/')))
 require("./middleware").defineMW(app)
 
 app.resource('services', require("./resources/service"))
@@ -30,14 +31,42 @@ app.post(CONFIG.startConnUrl, function (req, res) {
     })
 });
 
-app.get("/", function (req, res) {
-    res.sendfile("server/views/index.html")
+app.get("/ui/services", function (req, res) {
+    res.sendfile(path.resolve(__dirname, "views/index.html"))
 })
 
 app.get("/metadata", function (req, res) {
     res.sendfile("swagger.yaml")
 })
 
+app.get("/connector", function (req, res) {
+    res.sendfile(path.resolve(__dirname, "views/connector.html"))
+})
+app.post("/register", (req, res) => {
+    if (!req.body) res.sendStatus(400)
+    require("../prestart").generatePrivateKey() //openssl genrsa -out keys/ec-default.key 2048
+
+    endpointConfig = path.resolve("varkes.config.json")
+    var endpointsJson = JSON.parse(fs.readFileSync(endpointConfig))
+    console.log(endpointsJson)
+    var token = req.body.token
+    var hostname = req.body.hostname || "http://localhost"
+
+    createKeysFromToken(token, urls => {
+        fs.writeFileSync(path.resolve(CONFIG.keyDir, CONFIG.apiFile), JSON.stringify(urls), "utf8")
+
+        CONFIG.URLs = urls
+        console.log(urls)
+        keyFile = path.resolve(CONFIG.keyDir, 'ec-default.key')
+            , certFile = path.resolve(CONFIG.keyDir, 'kyma.crt')
+
+
+        createServicesFromConfig(hostname, endpointsJson)
+        res.send(`${endpointsJson.apis.length} services registered.`)
+    })
+
+
+})
 var server = app.listen(CONFIG.port, function () {
     var host = server.address().address
     var port = server.address().port
@@ -45,4 +74,80 @@ var server = app.listen(CONFIG.port, function () {
     console.log("App connector listening at http://%s:%s", host, port)
 
 });
+
+function createKeysFromToken(tokenUrl, cb) {
+
+    connector.exportKeys(tokenUrl, (data) => cb(data))
+}
+
+function createServicesFromConfig(hostname, endpoints) {
+
+    createSingleService(hostname, endpoints, 0)
+}
+
+
+function createSingleService(hostname, endpoints, endpointCount) {
+    serviceMetadata = defineServiceMetadata()
+    var element = endpoints.apis[endpointCount]
+    serviceMetadata.name = endpoints.name + "-" + Math.random().toString(36).substring(2, 5);
+    serviceMetadata.api.targetUrl = hostname + element.baseurl
+    serviceMetadata.api.credentials.oauth.url = hostname + element.oauth
+
+
+    request.post({
+        url: CONFIG.URLs.metadataUrl,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        json: serviceMetadata,
+        agentOptions: {
+            cert: fs.readFileSync(certFile),
+            key: fs.readFileSync(keyFile)
+        }
+    }, function (error, httpResponse, body) {
+        console.log(body)
+
+        if (endpointCount + 1 < endpoints.apis.length) {
+            createSingleService(hostname, endpoints, endpointCount + 1)
+        }
+    });
+}
+
+
+function defineServiceMetadata() {
+    return {
+        "provider": "aY",
+        "name": "ec-mock-service-4",
+        "description": "testing... 1.2.3.",
+        "api": {
+            "targetUrl": "http://localhost/target",
+            "credentials": {
+                "oauth": {
+                    "url": "http://localhost/oauth/validate",
+                    "clientId": "string",
+                    "clientSecret": "string"
+                }
+            },
+            "spec": {}
+        },
+        "events": {
+            "spec": {}
+        },
+        "documentation": {
+            "displayName": "string",
+            "description": "string",
+            "type": "string",
+            "tags": [
+                "string"
+            ],
+            "docs": [
+                {
+                    "title": "string",
+                    "type": "string",
+                    "source": "string"
+                }
+            ]
+        }
+    }
+}
 module.exports = server
