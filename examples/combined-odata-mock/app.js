5103a82c59a4e1b4c3c9ab17ad0679b66d9361e3
Elbadrawi <omar.elbadrawi@sap.com>
2018-12-19 10:11:44 +0100

    updating packages

--- examples/combined-odata-mock/app.js
+++ examples/combined-odata-mock/app.js
@@ -1,11 +1,4 @@
 var path = "C:\\Users\\D074188\\Desktop\\varkes\\examples\\combined-odata-mock\\config.js"
 var Resource = require('express-resource')
 var app = require("varkes-odata-mock")(path)
-//app.middleware('routes:before', Resource);
 app = require("varkes-app-connector-client")(app, path, true);
-// if (app) {
-
-//     app.listen(8000, function () {
-//         app.startLoopback();
-//     });
-// }
