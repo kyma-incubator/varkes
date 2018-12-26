fd09dd25454133f9644b579e8324b0672a8634ed
Elbadrawi <omar.elbadrawi@sap.com>
2018-12-26 14:51:30 +0100

    change spec_path to relative path

--- odata-mock/config.js
+++ odata-mock/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apis: [
-        { name: "marketing", metadata: "/marketing/metadata", specification_file: "C:\\Users\\D074188\\Desktop\\varkes\\odata-mock\\EDMX_MODEL_SPECIFICATION.xml" }
+        { name: "marketing", metadata: "/marketing/metadata", specification_file: "EDMX_MODEL_SPECIFICATION.xml" }
     ],
     storage_file_path: "data.json",
 
