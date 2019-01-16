dafaccf90291033792c65f9ac673f151e37a31cb
Thaler, Andreas <andreas.thaler01@sap.com>
2019-01-16 22:49:33 +0100

    bla

--- examples/combined-openapi-mock/varkes_config.js
+++ examples/combined-openapi-mock/varkes_config.js
@@ -20,5 +20,15 @@ module.exports = {
             specification_file: 'apis/courses.yaml',
             oauth: "/authorizationserver/oauth/token"
         }
+    ],
+    events: [
+        {
+            specification_file: "apis/events.json",
+            name: "events",
+            description: "All Events v1",
+            labels: {
+                "connected-app": "testApp"
+            }
+        }
     ]
 }
