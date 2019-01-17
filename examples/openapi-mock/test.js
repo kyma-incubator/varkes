da760a67fd7ea2c1d751c10799934f567f4d865d
Thaler, Andreas <andreas.thaler01@sap.com>
2019-01-17 13:13:37 +0100

    fixed test

--- examples/openapi-mock/test.js
+++ examples/openapi-mock/test.js
@@ -1,13 +1,13 @@
 var request = require('supertest');
 var openapiApp = require("varkes-openapi-mock")("./varkes_config.js")
 
-describe('tests odata controllers', function () {
+describe('tests openapi controllers', function () {
     it('should work', function () {
         openapiApp.then(function (app) {
-            describe('GET course 1 via API', function () {
+            describe('GET courses', function () {
                 it('should return 200', function (done) {
                     request(app)
-                        .get('/api1/courses/1')
+                        .get('/api1/courses')
                         .set('Accept', 'application/json')
                         .expect('Content-Type', 'application/json; charset=utf-8')
                         .expect(200,done)
@@ -17,8 +17,7 @@ describe('tests odata controllers', function () {
                 it('should return 200', function (done) {
                     request(app)
                         .get('/api1/console')
-                        .set('Accept', 'application/json')
-                        .expect('Content-Type', 'application/json; charset=utf-8')
+                        .expect('Content-Type', 'text/html; charset=utf-8')
                         .expect(200,done)
                 });
             });
