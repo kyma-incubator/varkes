a45150a76e1828f52f1c7014159182d6a5f5d8e3
Andreas Thaler <andreas.thaler01@sap.com>
2019-02-10 19:49:03 +0100

    introduced openapi3 support by transformation to swagger2 always

--- openapi-mock/server/app.js
+++ openapi-mock/server/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var middleware = require('swagger-express-middleware');
 const LOGGER = require("./logger").logger
 const bodyParser = require('body-parser');
-var mock_controller = require("./mock_controller");
+var mock = require("./mock");
 var Resource = require("express-resource")
 var app = require("express")();
 const path = require("path")
 const fs = require('fs');
 
-var middlewares = [];
 var varkesConfig
+const OAUTH = "/authorizationserver/oauth/token";
+const METADATA = "/metadata";
 
-module.exports = function (varkesConfigPath) {
+module.exports = async function (varkesConfigPath) {
   if (varkesConfigPath) {
     var endpointConfig = path.resolve(varkesConfigPath)
     LOGGER.info("Using configuration %s", endpointConfig)
@@ -26,29 +26,8 @@ module.exports = function (varkesConfigPath) {
   app.config = varkesConfig;
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }))
-  mock_controller.init(app, varkesConfig);
-  mock_controller.recordRequest(app);
 
-  let myDB = new middleware.MemoryDataStore();
-
-  for (var i = 0; i < varkesConfig.apis.length; i++) {
-    middlewares.push(
-      middleware(varkesConfig.apis[i].specification, app, function (err, middleware) {
-        app.use(
-          middleware.metadata(),
-          middleware.CORS(),
-          middleware.files(),
-          middleware.parseRequest(),
-          middleware.validateRequest(),
-          middleware.mock(myDB),
-        );
-        mock_controller.customErrorResponses(app)
-      })
-    );
-  }
-  return new Promise(function (resolve, reject) {
-    resolve(app)
-  });
+  return mock(app, varkesConfig);
 }
 
 function configValidation(configJson) {
@@ -59,8 +38,14 @@ function configValidation(configJson) {
       if (!api.name) {
         error_message += "\napi number " + i + ": missing attribute 'name', a name is mandatory";
       }
-      if(!api.type){
-        api.type="openapi"
+      if (!api.type) {
+        api.type = "openapi"
+      }
+      if (!api.oauth) {
+        api.oauth = OAUTH
+      }
+      if (!api.metadata) {
+        api.metadata = METADATA
       }
       if (!api.type.match(/^(openapi|odata)$/)) {
         error_message += "\napi '" + api.name + "': type '" + api.type + "' is not matching the pattern '^(openapi|odata)$'";
--- openapi-mock/test/test.js
+++ openapi-mock/test/test.js
@@ -1,10 +1,10 @@
 var request = require('supertest');
-var app = require("express")();
 var server = require('../server/app')('./test/varkes_config.json');
+
 describe('controllers', function () {
-  it('should work', function () {
+  it('should work', function (done) {
     server.then(function (app) {
-      describe('GET metadata for pets', function () {
+      describe('GET metadata for default metadata endpoint', function () {
         it('should return response 200', function (done) {
           request(app)
             .get("/api1/metadata")
@@ -13,7 +13,16 @@ describe('controllers', function () {
         });
       })
 
-      describe('GET json metadata for pets', function () {
+      describe('GET metadata for custom metatdata endpoint', function () {
+        it('should return response 200', function (done) {
+          request(app)
+            .get("/api5/mymetadata")
+            .expect('Content-Type', 'text/x-yaml; charset=utf-8')
+            .expect(200, done)
+        });
+      });
+
+      describe('GET json metadata for default metadata endpoint', function () {
         it('should return response 200', function (done) {
           request(app)
             .get("/api1/metadata.json")
@@ -22,7 +31,16 @@ describe('controllers', function () {
         });
       })
 
-      describe('GET yaml metadata for pets', function () {
+      describe('GET json metadata for custom metatdata endpoint', function () {
+        it('should return response 200', function (done) {
+          request(app)
+            .get("/api5/mymetadata.json")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, done)
+        });
+      });
+
+      describe('GET yaml metadata for default metadata endpoint', function () {
         it('should return response 200', function (done) {
           request(app)
             .get("/api1/metadata.yaml")
@@ -31,28 +49,38 @@ describe('controllers', function () {
         });
       })
 
-      describe('GET console for pets', function () {
+      describe('GET yaml metadata for ustom metatdata endpoint', function () {
+        it('should return response 200', function (done) {
+          request(app)
+            .get("/api5/mymetadata.yaml")
+            .expect('Content-Type', 'text/x-yaml; charset=utf-8')
+            .expect(200, done)
+        });
+      });
+
+      describe('GET console for default metadata endpoint', function () {
         it('should return response 200', function (done) {
           request(app)
             .get("/api1/console")
             .expect('Content-Type', 'text/html; charset=utf-8')
             .expect(200, done)
             .expect(/'\/api1\/metadata.json'/)
-            .expect(/pets v2/)
+            .expect(/pets openapi yaml/)
+        });
+      })
+    
+      describe('GET console for custom metadata endpoint', function () {
+        it('should return response 200', function (done) {
+          request(app)
+            .get("/api5/console")
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .expect(200, done)
+            .expect(/'\/api5\/mymetadata.json'/)
+            .expect(/courses/)
         });
       })
 
-      describe('GET collection for pets', function () {
-        it('should return response 200', function (done) {
-          request(app)
-            .get("/api1/pets")
-            .set('Accept', 'application/json')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200, done)
-        });
-      });
-
-      describe('GET oauth for pets', function () {
+      describe('GET oauth for default oauth endpoint', function () {
         it('should return response 200', function (done) {
           request(app)
             .post("/api1/authorizationserver/oauth/token")
@@ -63,37 +91,10 @@ describe('controllers', function () {
         });
       });
 
-      describe('GET metadata for courses', function () {
+      describe('GET oauth for custom oauth endpoint', function () {
         it('should return response 200', function (done) {
           request(app)
-            .get("/api2/mymetadata")
-            .expect('Content-Type', 'text/x-yaml; charset=utf-8')
-            .expect(200, done)
-        });
-      });
-
-      describe('GET json metadata for courses', function () {
-        it('should return response 200', function (done) {
-          request(app)
-            .get("/api2/mymetadata.json")
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200, done)
-        });
-      });
-
-      describe('GET yaml metadata for courses', function () {
-        it('should return response 200', function (done) {
-          request(app)
-            .get("/api2/mymetadata.yaml")
-            .expect('Content-Type', 'text/x-yaml; charset=utf-8')
-            .expect(200, done)
-        });
-      });
-
-      describe('GET oauth for courses', function () {
-        it('should return response 200', function (done) {
-          request(app)
-            .post("/api2/myoauth/token")
+            .post("/api5/myoauth/token")
             .send({client_id:"1",client_secret:"2",grant_type:"3"})
             .set('Accept', 'application/json')
             .expect('Content-Type', 'application/json; charset=utf-8')
@@ -101,26 +102,89 @@ describe('controllers', function () {
         });
       });
 
-      describe('GET console for courses', function () {
+      describe('GET validated request for openapi yaml', function () {
         it('should return response 200', function (done) {
           request(app)
-            .get("/api2/console")
-            .expect('Content-Type', 'text/html; charset=utf-8')
-            .expect(200, done)
-            .expect(/'\/api2\/mymetadata.json'/)
-            .expect(/courses/)
-        });
-      })
-
-      describe('GET collection for courses', function () {
-        it('should return response 200', function (done) {
-          request(app)
-            .get("/api2/courses")
+            .get("/api1/pets?Type=dog")
             .set('Accept', 'application/json')
             .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(/\[\]/)
             .expect(200, done)
         });
       });
-    })
+
+      describe('GET invalid request for openapi yaml', function () {
+        it('should return response 400', function (done) {
+          request(app)
+            .get("/api1/pets")
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400, done)
+        });
+      });
+
+      describe('GET validated request for openapi json', function () {
+        it('should return response 200', function (done) {
+          request(app)
+            .get("/api2/pets?Type=dog")
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(/\[\]/)
+            .expect(200, done)
+        });
+      });
+
+      describe('GET invalid request for openapi json', function () {
+        it('should return response 400', function (done) {
+          request(app)
+            .get("/api2/pets")
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400, done)
+        });
+      });
+
+      describe('GET validated request for swagger yaml', function () {
+        it('should return response 200', function (done) {
+          request(app)
+            .get("/api3/pets?type=dog")
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(/\[\]/)
+            .expect(200, done)
+        });
+      });
+
+      describe('GET invalid request for swagger yaml', function () {
+        it('should return response 400', function (done) {
+          request(app)
+            .get("/api3/pets")
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400, done)
+        });
+      });
+
+      describe('GET validated request for swagger json', function () {
+        it('should return response 200', function (done) {
+          request(app)
+            .get("/api4/pets?type=dog")
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(/\[\]/)
+            .expect(200, done)
+        });
+      });
+
+      describe('GET invalid request for swagger json', function () {
+        it('should return response 400', function (done) {
+          request(app)
+            .get("/api4/pets")
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400, done)
+        });
+      });
+    }).finally(done);
   })
 });
