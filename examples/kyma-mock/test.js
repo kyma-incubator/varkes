c194c10efd902960195770fc379655444fc3a299
Elbadrawi <omar.elbadrawi@sap.com>
2019-02-18 17:53:23 +0100

    creating Kyma example

---
+++ examples/kyma-mock/test.js
@@ -0,0 +1,26 @@
+var request = require('supertest');
+var exampleApp = require("./app.js")
+
+describe('tests openapi controllers', function () {
+    it('should work', function (done) {
+        exampleApp.then(function (app) {
+            describe('GET courses', function () {
+                it('should return 200', function (done) {
+                    request(app)
+                        .get('/api1/courses')
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type', 'application/json; charset=utf-8')
+                        .expect(200,done)
+                });
+            });
+            describe('GET console', function () {
+                it('should return 200', function (done) {
+                    request(app)
+                        .get('/api1/console')
+                        .expect('Content-Type', 'text/html; charset=utf-8')
+                        .expect(200,done)
+                });
+            });
+        }).finally(done);
+    });
+});
