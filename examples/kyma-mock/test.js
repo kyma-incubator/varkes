a4bdd9e2ca4d6068fde062ad9fc3a2d3b3065e0c
Elbadrawi <omar.elbadrawi@sap.com>
2019-02-20 12:38:13 +0100

    adding all kyma api specifications

--- examples/kyma-mock/test.js
+++ examples/kyma-mock/test.js
@@ -1,22 +1,50 @@
 var request = require('supertest');
 var exampleApp = require("./app.js")
 
-describe('tests openapi controllers', function () {
+describe('tests kyma controllers', function () {
     it('should work', function (done) {
         exampleApp.then(function (app) {
-            describe('GET courses', function () {
+            describe('GET connector api health', function () {
                 it('should return 200', function (done) {
                     request(app)
-                        .get('/kyma/courses')
-                        .set('Accept', 'application/json')
-                        .expect('Content-Type', 'application/json; charset=utf-8')
+                        .get('/connectorapi/v1/health')
                         .expect(200, done)
                 });
             });
-            describe('GET console', function () {
+            describe('GET events api health', function () {
                 it('should return 200', function (done) {
                     request(app)
-                        .get('/kyma/console')
+                        .get('/eventsapi/v1/health')
+                        .expect(200, done)
+                });
+            });
+            describe('GET metadata api health', function () {
+                it('should return 200', function (done) {
+                    request(app)
+                        .get('/metadataapi/v1/health')
+                        .expect(200, done)
+                });
+            });
+            describe('GET connector api console', function () {
+                it('should return 200', function (done) {
+                    request(app)
+                        .get('/connectorapi/console')
+                        .expect('Content-Type', 'text/html; charset=utf-8')
+                        .expect(200, done)
+                });
+            });
+            describe('GET events api console', function () {
+                it('should return 200', function (done) {
+                    request(app)
+                        .get('/eventsapi/console')
+                        .expect('Content-Type', 'text/html; charset=utf-8')
+                        .expect(200, done)
+                });
+            });
+            describe('GET metadata api console', function () {
+                it('should return 200', function (done) {
+                    request(app)
+                        .get('/metadataapi/console')
                         .expect('Content-Type', 'text/html; charset=utf-8')
                         .expect(200, done)
                 });
