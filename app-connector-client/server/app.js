d3c04ecb2ceb658c05ae06d9c03d2400f7027cbf
Elbadrawi <omar.elbadrawi@sap.com>
2019-03-01 14:46:55 +0100

    cleaning up unwanted code

--- app-connector-client/server/app.js
+++ app-connector-client/server/app.js
@@ -13,10 +13,6 @@ const connector = require("./routes/connector");
 const events = require("./routes/events")
 var apis = require("./routes/apis")
 var keys = require("./keys")
-const check_api = require('check_api');
-const yaml = require('js-yaml');
-var app = express()
-var varkesConfig
 
 module.exports = function (varkesConfigPath = null, nodePortParam = null) {
     var app = express()
@@ -51,40 +47,4 @@ module.exports = function (varkesConfigPath = null, nodePortParam = null) {
     return new Promise(function (resolve, reject) {
         resolve(app)
     });
-}
-
-function configValidation(configJson) {
-    var error_message = "";
-
-    var events = configJson.events;
-    if (events) {
-        for (var i = 1; i <= events.length; i++) {
-            {
-                var event = events[i - 1];
-                var specInJson
-                if (event.specification.endsWith(".json")) {
-                    specInJson = JSON.parse(fs.readFileSync(event.specification))
-                } else {
-                    specInJson = yaml.safeLoad(fs.readFileSync(event.specification, 'utf8'));
-                }
-                check_api.check_api(specInJson, {}, function (err, options) {
-                    if (err) {
-                        error_message += "\nevent number " + i + ": Schema validation Error \n" + JSON.stringify(err)
-                    }
-                })
-                if (!event.name) {
-                    error_message += "\nevent number " + i + ": missing attribute 'name', a name is mandatory";
-                }
-                if (!event.specification) {
-                    error_message += "\nevent '" + event.name + "': missing attribute 'specification', a specification is mandatory";
-                }
-                if (!event.specification.match(/^.+\.(json|yaml|yml)$/)) {
-                    error_message += "\nevent '" + event.name + "': specification '" + event.specification + "' does not match pattern '^.+\\.(json|yaml|yml)$'";
-                }
-            }
-        }
-    }
-    if (error_message != "") {
-        throw new Error("Validation of configuration failed: " + error_message);
-    }
 }
