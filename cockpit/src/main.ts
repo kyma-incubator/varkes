4c0d772a509125837eeefb5706e8a4533e47c333
Elbadrawi <omar.elbadrawi@sap.com>
2019-03-28 14:39:46 +0100

    remove express from main

--- cockpit/src/main.ts
+++ cockpit/src/main.ts
@@ -3,8 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import * as express from 'express';
-var app = express();
 if (environment.production) {
   enableProdMode();
 }
