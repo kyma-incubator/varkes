5fb9674ff440b684a217bcac73d2d6818c401423
Elbadrawi <omar.elbadrawi@sap.com>
2019-04-04 19:20:27 +0200

    add connection to kyma to UI

--- cockpit/src/app/app.component.ts
+++ cockpit/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import {
-  Component
-} from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'varkes-ui-app';
+  public info;
+  public loadViews;
+  public constructor(private http: Http) {
+    let hostname;
+    if (window["config"] && window["config"].domain) {
+      hostname = window["config"].domain;
+    }
+    else {
+      hostname = window.location.origin;
+    }
+    this.http.get(hostname + "/info")
+      .subscribe(
+        data => {
+          window['info'] = JSON.parse(data["_body"]);
+          this.loadViews = true;
+        });
+  }
 }
