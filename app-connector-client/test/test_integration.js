11c3f2af2257434fab585e95c141e38d97993d59
Elbadrawi <omar.elbadrawi@sap.com>
2019-04-12 10:10:59 +0200

    fixing app-connector tests

--- app-connector-client/test/test_integration.js
+++ app-connector-client/test/test_integration.js
@@ -74,9 +74,9 @@ describe("should work", () => {
             return request(server)
                 .get('/remote/apis')
                 .expect(200)
-                .expect(/"provider":"schoolProvider","name":"schools","description":"Schools Webservices","labels":{"label1":"value1"}/)
-                .expect(/"api":{"targetUrl":"http:\/\/localhost","credentials":{"oauth":{"url":"http:\/\/localhost\/entity\/schoolToken","clientId":"admin","clientSecret":"nimda","csrfInfo":{"tokenEndpointURL":"http:\/\/localhost\/entity"}}},"specificationUrl":"http:\/\/localhost\/entity\/schoolMetadata",/)
-                .expect(/"provider":"Varkes","name":"courses","description":"Courses Webservices","labels":{}/)
+                .expect(/"provider":"schoolProvider","name":"schools","description":"Schools Webservices","labels":{"label1":"value1","type":"OpenAPI"}/)
+                .expect(/"api":{"targetUrl":"http:\/\/localhost\/entity","credentials":{"oauth":{"url":"http:\/\/localhost\/entity\/schoolToken","clientId":"admin","clientSecret":"nimda"}},"specificationUrl":"http:\/\/localhost\/entity\/schoolMetadata",/)
+                .expect(/"provider":"Varkes","name":"courses","description":"Courses Webservices","labels":{"type":"OpenAPI"}/)
                 .expect(/"api":{"targetUrl":"http:\/\/localhost\/entity\/v1","credentials":{"basic":{"username":"admin","password":"nimda"}},"specificationUrl":"http:\/\/localhost\/entity\/v1\/metadata"/)
 
         })
@@ -85,8 +85,8 @@ describe("should work", () => {
             return request(server)
                 .get('/remote/apis')
                 .expect(200)
-                .expect(/"provider":"myProvider","name":"events1","description":"All Events v1","labels":{"label1":"value1"}/)
-                .expect(/"provider":"Varkes","name":"events2","description":"All Events v2","labels":{}/)
+                .expect(/"provider":"myProvider","name":"events1","description":"All Events v1","labels":{"label1":"value1","type":"AsyncApi"}/)
+                .expect(/"provider":"Varkes","name":"events2","description":"All Events v2","labels":{"type":"AsyncApi"}/)
         })
 
         it('404 everything else', () => {
