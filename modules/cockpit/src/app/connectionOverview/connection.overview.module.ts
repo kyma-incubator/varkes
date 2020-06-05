e8ab31532083eb4f1288449296c69e72812cb1b5
Jamie Cawley <jamie.cawley@gmail.com>
2020-06-05 04:56:11 -0400

    loading spinner (#277)
    Co-authored-by: Andreas Thaler <andreas.thaler01@sap.com>

--- modules/cockpit/src/app/connectionOverview/connection.overview.module.ts
+++ modules/cockpit/src/app/connectionOverview/connection.overview.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ConnectionRoutingModule } from './connection-routing.module';
-import { ConnectionOverviewComponent } from './connection.overview';
-import { ApiTableModule } from '../apitable/apitable.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ConnectionRoutingModule } from "./connection-routing.module";
+import { ConnectionOverviewComponent } from "./connection.overview";
+import { ApiTableModule } from "../apitable/apitable.module";
+import { LoadingSpinnerModule } from "fundamental-ngx";
+
 @NgModule({
-    declarations: [
-        ConnectionOverviewComponent
-    ],
-    imports: [
-        BrowserModule,
-        ConnectionRoutingModule,
-        ApiTableModule
-    ],
-    exports: [ConnectionOverviewComponent],
-    providers: [],
-    bootstrap: []
+  declarations: [ConnectionOverviewComponent],
+  imports: [
+    BrowserModule,
+    ConnectionRoutingModule,
+    ApiTableModule,
+    LoadingSpinnerModule,
+  ],
+  exports: [ConnectionOverviewComponent],
+  providers: [],
+  bootstrap: [],
 })
-export class ConnectionOverviewModule { }
+export class ConnectionOverviewModule {}
