ad36265c979582ef5f85e28f96997a81bd2db665
Thaler, Andreas <andreas.thaler01@sap.com>
2020-10-31 21:54:05 +0100

    enabled non-colored json logs for docker deployments

--- modules/configuration/src/logger.ts
+++ modules/configuration/src/logger.ts
@@ -4,7 +4,7 @@
 import * as winston from "winston"
 
 export function logger(loggerLabel: string): winston.Logger {
-    const console_logger = new winston.transports.Console({
+    const local_logger = new winston.transports.Console({
         format: winston.format.combine(
             winston.format.colorize(),
             winston.format.label({ label: loggerLabel }),
@@ -12,16 +12,17 @@ export function logger(loggerLabel: string): winston.Logger {
         )
     })
 
-    const file_logger = new winston.transports.File({ filename: process.env.NODE_ENV ? `logs/${process.env.NODE_ENV}.log` : 'logs/server.log' })
-
+    const production_logger = new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.label({ label: loggerLabel }),
+            winston.format.json()
+        )
+    })
     const logger = winston.createLogger({
         level: process.env.DEBUG ? 'debug' : 'info',
-        format: winston.format.combine(
-            winston.format.splat(),
-            winston.format.json()
-        ),
+        format: winston.format.splat(),
         transports: [
-            process.env.NODE_ENV === "test" ? file_logger : console_logger
+            process.env.NODE_ENV === "production" ? production_logger : local_logger
         ]
     })
     return logger
